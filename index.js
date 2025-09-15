d3d04aa81593f503e9d2693717c65a42edaafd99
Priyansh <priyanshv03@gmail.com>
2025-09-15 13:14:48 +0530

    modified

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.listen(port, (req, res) => {
-  console.log(`server is listening at ${port}`);
+  console.log(`server is running at ${port}`);
 });
